169d461ce3c09d03d6afcabefb1c4a28cc9aae8a
yuliujpl <david.liu@jpl.nasa.gov>
2018-06-09 14:04:39 -0700

    Removed artifact linkage between publication and author.

--- ksdb/management/commands/publishrdfs.py
+++ ksdb/management/commands/publishrdfs.py
@@ -1,7 +1,7 @@
 #publishPublication.rdf
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from django.core.management.base import BaseCommand, CommandError
-from ksdb.models import publication, publication_author_link, person, protocol, pi_protocol_link, organ_protocol_link, person_degree_link, degree, program, institution, institution_personnel_link, fundedsite, fundedsite_staff_link, fundedsite_pi_link, fundedsite_organ_link, fundedsite_program_link, fundedsite_institution_link, organ, species, specimentype, discipline, IdSeq, disease, group, group_member_link, con_fundedsite_link, protocol_custodian_link,protocol_publication_link, fundedsite_protocol_link, group_program_link, ci_protocol_link, committee, committee_member_link, committee_program_link, group_chair_link, group_cochair_link, committee_chair_link, committee_cochair_link, protocol_program_link, publication_program_link, pi_publication_link
+from ksdb.models import publication, person, protocol, pi_protocol_link, organ_protocol_link, person_degree_link, degree, program, institution, institution_personnel_link, fundedsite, fundedsite_staff_link, fundedsite_pi_link, fundedsite_organ_link, fundedsite_program_link, fundedsite_institution_link, organ, species, specimentype, discipline, IdSeq, disease, group, group_member_link, con_fundedsite_link, protocol_custodian_link,protocol_publication_link, fundedsite_protocol_link, group_program_link, ci_protocol_link, committee, committee_member_link, committee_program_link, group_chair_link, group_cochair_link, committee_chair_link, committee_cochair_link, protocol_program_link, publication_program_link, pi_publication_link
 from ksdb.forms import PublicationForm
 from ksdb.ekeutils import format_phone, strip_syms
 
